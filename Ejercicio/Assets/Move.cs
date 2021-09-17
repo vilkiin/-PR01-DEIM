46c9fc26f16255a6a545909e868f938e7fbc30f2
vilkiin <jorgeeldiaz08@gmail.com>
2021-09-17 17:48:55 +0100

    ejercicio 1b
    publicas y declaradas , cambiables en unity y declaradas en start

--- Ejercicio/Assets/Move.cs
+++ Ejercicio/Assets/Move.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-
-    private int num0 = 8;
-    private int num1 = 20;
-    private float num2 =7.5f;
-    private float num3 = 4.3f;
-    private string texto = "Hola mundo";
-    private bool vida = true;
+    // valor al momento de declararlos
+    public int num0 = 8;
+    public int num1 = 20;
+    public float num2 =7.5f;
+    public float num3 = 4.3f;
+    public string texto = "Hola mundo";
+    public bool vida = true;
     
-    
+    // valor cambiable desde unity
+    public int num10;
+    public int num11;
+    public float num12;
+    public float num13;
+    public string escrito;
+    public bool life;
 
+    // valor en start
+    public int num20;
+    public int num21;
+    public float num22;
+    public float num23;
+    public string palabras;
+    public bool die;
 
     // Start is called before the first frame update
     void Start()
@@ -36,11 +49,25 @@ public class Move : MonoBehaviour
 
             print("estas muerto");
         }
-       
-        
-        
+
+        // valor cambiable desde unity
+        print(num10);
+        print(num11);
+        print(num12);
+        print(num13);
+        print(escrito);
+        print(life);
 
 
+        // valor en start
+        print(num20 = 52);
+        print(num21 = 23);
+        print(num22 = 56.9f);
+        print(num23 = 13.23f);
+        print(palabras = "The End");
+        print(die = true);
+        
+        
 
     }
 
